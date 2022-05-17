dc3e97061545bb2c6bb0bce593989375d0e51a36
Mizanur Rahman <mizanur.rahman@navana.com>
2022-05-17 21:12:59 +0600

    simple increment and decrement working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 
 import React from "react";
 
+const reducerFunction = (state, action) => {
+
+  switch (action.type) {
+    case 'increament':
+      return {
+        ...state, 
+        count: state.count + 1
+      }
+      
+    case 'decrement': 
+      return {
+      ...state, 
+      count: state.count - 1
+    }
+  
+    default:
+      return state
+  }
+
+} 
+
+const initialState = {
+  count: 0
+}
+
 function App() {
 
   const [input, setInput] = React.useState(0)
+  const [state, dispatch] = React.useReducer(reducerFunction, initialState)
   
  return (
    <div className="app">
@@ -17,6 +43,15 @@ function App() {
        <br/>
        <button>Initalize counter</button>
      </div>
+
+     <p>{state.count}</p>
+
+     <button onClick={() => dispatch({
+       type: 'increament'
+     })}>increament</button>
+     <button onClick={() => dispatch({
+       type: 'decrement'
+     })}>decreament</button>
    </div>
  )
 }
